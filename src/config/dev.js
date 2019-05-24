37dba18f32b9f0c33bf87481e895a5115ea1057b
dchavours <30337368+dchavours@users.noreply.github.com>
2019-05-24 07:05:42 -0500

    Delete dev.js

--- src/config/dev.js
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-  apiKey: "AIzaSyCiNBQ6_V7a2V7wpvigxFP3MsZgdT3fvNk",
-  authDomain: "kw-graphics-server.firebaseapp.com",
-  databaseURL: "https://kw-graphics-server.firebaseio.com",
-  projectId: "kw-graphics-server",
-  storageBucket: "kw-graphics-server.appspot.com",
-  messagingSenderId: "334742471890"
-}
