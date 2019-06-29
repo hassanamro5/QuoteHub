f98ca5618ffd89bbf28a42e57a59742c164a9d69
Dennis Chavours <dchavours@gmail.com>
2019-06-28 20:18:48 -0500

    added number menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import List from './components/List';
+import Badge from './components/Badge';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+        <Badge />
         <List/>
       </div>
     );
